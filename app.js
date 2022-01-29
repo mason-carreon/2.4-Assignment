642995f6eb0eb22edcfe5962d2c3a9a593ff52c3
mason-carreon <70252484+mason-carreon@users.noreply.github.com>
2022-01-28 18:23:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 {
-
-    let x="This";
+    let x="This is a let variable"; // This is a block variable
     console.log (x);
+    var y="This is a var variable"; // This is a global variable
     }
-    
+    console.log (y);
+
+    let a = 5;
+    let b = 6;
+
+    let z = a + b; // This is a mathematical operations with variables.
+
+    console.log ("The total is " + z);
+
+    {
+        const car = {type:"Mustang", model:"302", color:"red"}; // This is a object variable
+        console.log ("The car is a " + car.type);
+         }
+
+         let text = "This is a string using the \"let\" variable"; // This is a string
+
+         console.log (text);
